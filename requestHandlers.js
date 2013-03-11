b4fd6356760a42dbbcd6a1bc5b0a745e6c0171ad
Doctor Cobweb <spinninghalf@gmail.com>
2013-03-11 21:36:18 +1100

    app now handles POST queries and parses the post data base on the text area name attribute.

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,10 +1,25 @@
 var exec = require("child_process").exec;
+var querystring = require("querystring");
 
 
-function start(response) {
+//we are bringing the response object from the server TO the request handlers,
+//not returning content from the request handlers back to the server to respond.
+//instead, having the response object here allows the request handlers to 
+//operate aysnchronously (as node requires) whilst providing content back to the
+//browser once the callbacks are called and proper content available.
+//if we were to return content back to the server, and let it respond appropriately,
+//with its own response object, we run the risk of returning no content because the async
+//operation may not have finished (and thus populating the content to return).
+
+function start(response, postData) {
   console.log("*** Request handler 'start' was called. ***");
 
+  /*
   //exec("ls -lah", function (error, stdout, stderr) {
+  //"find /" is a really expensive action, taking time to complete.
+  //using this operation illustrates the async nature of node
+  //and it being to still be able to handle subsequent requests whilst
+  //"find /" is working in the background.
     exec("find /", 
       {timeout: 10000, maxBuffer: 20000*1024},
       function (error, stdout, stderr) {
@@ -12,12 +27,33 @@ function start(response) {
         response.write(stdout);
         response.end();
       });
+   */
+
+
+    var body = '<html>' +
+      '<head>' +
+      '<meta http-equiv="Content-Type" content="text/html" ' +
+      'charset=UTF-8" /> ' +
+      '</head>' +
+      '<body>' +
+      '<form action="/upload" method="POST">' +
+      '<textarea name="textie" rows="20" cols="60"></textarea>' +
+      '<input type="submit" value="Submit text"/>' +
+      '</form>' +
+      '</body>' +
+      '</html>';
+
+      response.writeHead(200, {"Content-Type":"text/html"});
+      response.write(body);
+      response.end();
 }
 
-function upload(response) {
+function upload(response, postData) {
   console.log("*** Request handler 'upload' was called. ***");
   response.writeHead(200, {"Content-Type":"text/plain"});
-  response.write("Hello upload URL");
+  //response.write("POST DATA RECEIVED: " + postData);
+  response.write("You've sent the text: " + 
+  querystring.parse(postData).textie);
   response.end();
 }
 
--- server.js
+++ server.js
@@ -3,10 +3,26 @@ var url = require('url');
 
 function start(route, handle) {
   function onRequest(request, response){
+      var postData = "";
       var pathname = url.parse(request.url).pathname;
       console.log("Request for " + pathname + " received.");
 
-      route(handle, pathname, response);
+      request.setEncoding("utf-8");
+    
+      //to listen to events use 'request.on' or 'request.addListener'
+      //request.addListener("data", function(postDataChunk){
+      request.on("data", function(postDataChunk){
+        postData += postDataChunk;
+        console.log("Received POST data chunk '" + postDataChunk + "'.");
+      });
+
+      //request.addListener("end", function(){
+      request.on("end", function(){
+        route(handle, pathname, response, postData);
+
+      });
+
+      //route(handle, pathname, response);
 
       //response.writeHead(200, {'Content-Type': 'text/plain'});
       //response.write('Hello World!');
